a41f3efbfc9585a1095ce238ee9a7b6db1e06675
NIkolay Koev <nikslotar@abv.bg>
2023-04-16 23:36:26 +0300

    Add Contacts route

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,6 +2,7 @@ const homeController = require('../controllers/homeController');
 const authController = require('../controllers/authController');
 const vehicleController = require('../controllers/vehicleController');
 const profileController = require('../controllers/profileController');
+const contactsController = require('../controllers/contactsController');
 
 
 module.exports = (app) => {
@@ -10,4 +11,5 @@ module.exports = (app) => {
     app.use('/api/auth', authController);
     app.use('/api/vehicles', vehicleController);
     app.use('/api/user/profile', profileController);
+    app.use('/api/contacts', contactsController);
 };
