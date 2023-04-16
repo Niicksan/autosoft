65f9c02c47d005910a979c6fd2087a319ce80fa6
NIkolay Koev <nikslotar@abv.bg>
2023-04-16 11:40:11 +0300

    Add Access-Control-Allow-Origin

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,7 +1,7 @@
 module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://autosoft.onrender.com, http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Authorization');
 
     next();
 };
