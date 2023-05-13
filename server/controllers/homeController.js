82598ea5031b39bab5509160ced690330d859d81
Niicksan <nikslotar@abv.bg>
2023-05-13 21:07:43 +0300

    Remove console logs

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -3,8 +3,6 @@ const homeController = require('express').Router();
 
 // TODO Replace with the real controller
 homeController.get('/', (req, res) => {
-    console.log('Home: ', req.user);
-    console.log('Home: ', req.token);
     res.send('<h1> REST is running!</h1>');
 });
 
